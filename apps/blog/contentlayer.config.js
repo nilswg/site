76c97c6874763ef0e1e911fbd509a274a3722bbc
nilswg <nilsonweng@gmail.com>
2024-10-10 17:58:04 +0800

    [update] .

---
+++ apps/blog/contentlayer.config.js
@@ -0,0 +1,60 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files"
+import highlight from 'rehype-highlight'
+import remarkGfm from 'remark-gfm'
+
+/** @type {import('contentlayer/source-files').ComputedFields} */
+const computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+  },
+  slugAsParams: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
+  },
+}
+
+export const Page = defineDocumentType(() => ({
+  name: "Page",
+  filePathPattern: `pages/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+    },
+  },
+  computedFields,
+}))
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `posts/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+    },
+    date: {
+      type: "date",
+      required: true,
+    },
+  },
+  computedFields,
+}))
+
+export default makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post, Page],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [highlight],
+  },
+})
